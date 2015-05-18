f1d573cc14840ecfb246c03967064c798836385d
sejong <umlstudy@gmail.com>
2015-05-18 12:29:13 +0900

    ...

--- src/main/java/com/hmkcode/spring/mybatis/AppConfig.java
+++ src/main/java/com/hmkcode/spring/mybatis/AppConfig.java
@@ -1,13 +1,17 @@
 package com.hmkcode.spring.mybatis;  
 
+import java.sql.Driver;
+
 import javax.sql.DataSource;
 
-import org.hsqldb.jdbc.JDBCDriver;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
@@ -19,6 +23,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.hmkcode.spring.mybatis.mapper.Mapper;
 import com.hmkcode.spring.mybatis.service.Service;
   
+@PropertySource(name="jdbc.properties", value="classpath:/jdbc.properties")
 @Configuration 
 @EnableTransactionManagement
 public class AppConfig {  
@@ -28,14 +33,27 @@ public class AppConfig {
 
 	@Value("classpath:db-test-data.sql")
 	private Resource dataScript;
+	
+	@Autowired
+    private Environment env;
 
+	@SuppressWarnings("unchecked")
 	@Bean
 	public SimpleDriverDataSource getDataSource() {
 		SimpleDriverDataSource simpleDriverDataSource = new SimpleDriverDataSource();
-		simpleDriverDataSource.setDriverClass(JDBCDriver.class);
-		simpleDriverDataSource.setUrl("jdbc:hsqldb:mem:testdb");
-		simpleDriverDataSource.setUsername("");
-		simpleDriverDataSource.setPassword("");
+//		simpleDriverDataSource.setDriverClass(JDBCDriver.class);
+//		simpleDriverDataSource.setUrl("jdbc:hsqldb:mem:testdb");
+//		simpleDriverDataSource.setUsername("");
+//		simpleDriverDataSource.setPassword("");
+		try {
+			simpleDriverDataSource.setDriverClass((Class<Driver>)Class.forName(env.getProperty("ds.driverClass")));
+			simpleDriverDataSource.setUsername(env.getProperty("ds.userName"));
+			simpleDriverDataSource.setPassword(env.getProperty("ds.password"));
+			simpleDriverDataSource.setUrl(env.getProperty("ds.url"));
+		} catch ( ClassNotFoundException e ) {
+			
+		}
+		
 		return simpleDriverDataSource;
 	}
 	
